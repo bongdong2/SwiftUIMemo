9a83ab74ab190aefcc90b16b823a2cf794b21de5
좌승의 <jsu373712@naver.com>
2022-09-14 13:31:32 +0900

    add new memo

--- Shared/View/MainListView.swift
+++ Shared/View/MainListView.swift
@@ -11,6 +11,9 @@ struct MainListView: View {
     // 하나의 데이터를 여러 뷰에서 공유하고 싶을 떄 이런 방식으로 사용합니다.
     @EnvironmentObject var store: MemoStore
     
+    // Compose View를 표시할 때 사용한다.
+    @State private var showComposer: Bool = false
+    
     var body: some View {
         NavigationView {
             List(store.list) { memo in
@@ -20,6 +23,19 @@ struct MainListView: View {
             }
             .listStyle(.plain)
             .navigationTitle("내 메모")
+            .toolbar {
+                // '+' 버튼을 오른쪽에 바로 추가하니까 ToolbarItemGroup 사용 안 하겠음
+                Button {
+                    showComposer = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            // sheet 모디파이더는 바인딩 속성에 true가 전달되면 내부 클로저가 실행되고 클로저에서 리턴한 뷰를 모달방식으로 표시해준다.
+            // 바인딩을 전달할 때는 딸라($) 문자열로 전달해야 한다.
+            .sheet(isPresented: $showComposer) {
+                ComposeView()
+            }
         }
     }
 }
