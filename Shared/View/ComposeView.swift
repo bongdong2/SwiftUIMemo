1ca5d31d6511c6e1ea5f4f124ff4b0ded135bd4f
좌승의 <jsu373712@naver.com>
2022-09-14 20:12:32 +0900

    edit memo

--- Shared/View/ComposeView.swift
+++ Shared/View/ComposeView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ComposeView: View {
     @EnvironmentObject var store: MemoStore
     
+    // 여기에 Memo가 전달되면 edit, nil이면 write
+    var memo: Memo? = nil
+    
     @Environment(\.dismiss) var dismiss
     
     // 이런걸 State Variable 이라고 합니다.
@@ -21,8 +24,14 @@ struct ComposeView: View {
                 // content 속성을 전달 - two-way binding
                 TextEditor(text: $content)
                     .padding()
+                    .onAppear() {
+                        // 화면이 표시되는 시점에 초기화코드를 넣고 싶다면 여기에 구현
+                        if let memo = memo {
+                            content = memo.content
+                        }
+                    }
             }
-            .navigationTitle("새 메모")
+            .navigationTitle(memo != nil ? "메모 편집" : "새 메모")
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
                 // 바로 버튼을 추가하면 오른쪽에 버튼이 생성되므로 그룹을 만든다.
@@ -36,7 +45,12 @@ struct ComposeView: View {
                 }
                 ToolbarItemGroup(placement: .navigationBarTrailing) {
                     Button {
-                        store.insert(memo: content)
+                        if let memo = memo {
+                            store.update(memo: memo, content: content)
+                        } else {
+                            store.insert(memo: content)
+                        }
+                        
                         dismiss()
                     } label: {
                         Text("저장")
