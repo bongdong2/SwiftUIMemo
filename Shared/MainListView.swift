f26aed3b94e2d82a94fb474680e337ef5e3e0d66
좌승의 <jsu373712@naver.com>
2022-09-14 11:20:31 +0900

    initial project

---
+++ Shared/MainListView.swift
@@ -0,0 +1,20 @@
+//
+//  MailListView.swift
+//  SwiftUIMemo
+//
+//  Created by 승의 on 2022/09/14.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MailListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainListView()
+    }
+}
